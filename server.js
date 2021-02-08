0b3713f4f4e63484cbd1c2054079c740b5c6a2bb
dec8768 <dec8768@g.rit.edu>
2021-02-07 21:30:51 -0500

    i dont even know

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const cors = require("cors");
 const app = express();
 const port = 4000; 
 const mongoose = require("mongoose")
+require("dotenv/config");
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://pikachungg:Fufupapachon23@cluster0.y72ni.mongodb.net/ACS", { useNewUrlParser: true },{ useUnifiedTopology: true })
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },{ useUnifiedTopology: true })
+
+const db = mongoose.connection;
+
+//Shows on error.
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//Makes sure it's open message
+db.once("open", () => {
+    console.log("connection success")
+});
 
 const participantSchema = new mongoose.Schema({
     username: String,
@@ -16,15 +27,20 @@ const participantSchema = new mongoose.Schema({
 
 const Participant = mongoose.model("Participant", participantSchema);
 
+const prizeSchema = new mongoose.Schema({
+    number: Number,
+    prize: String,
+});
+
+const Prize = mongoose.model("Prize", prizeSchema);
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.post("/register", async (req, res) => {
     const {username} = req.body;
-
     const participant = await Participant.findOne({username}).exec();
-
     if (participant){
         //console.log("particpant exist: ", participant)
         res.status(500);
@@ -33,12 +49,15 @@ app.post("/register", async (req, res) => {
         });
         return;
     }
-
     const prize = "";
     await Participant.create({username, prize});
     res.json({
         username
     });
+});
+
+app.get("/prize", (req, res) => {
+    res.send("this is prize");
 });
 
 app.listen(port, ()=> {
--- src/pages/Laisee.js
+++ src/pages/Laisee.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../assets/css/laisee.css';
 
 export default function Laisee() {
+    const [prize, setPrize] = useState("");
+
+    const getPrize = () => {
+        fetch(`http://localhost:4000/prize`, {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json", 
+            },
+            body: JSON.stringify({
+                prize,
+            }),
+        })
+    }
+
     return (
         <div>
             <div className="container">
-                <button>This is laisee</button>
+                <button onClick={getPrize}>This is laisee</button>
             </div>
         </div>
     )
